bfa559eec70680689ed468b4bbd5c390140ae22c
Joe <joe64gg@gmail.com>
2023-09-13 20:48:26 +0200

    fix(login): email login now working

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -2,7 +2,7 @@
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import logger from '@/lib/logger';
 
@@ -11,6 +11,24 @@ import { isProd } from '@/constant/env';
 export default function AuthForm() {
   const supabase = createClientComponentClient();
   const [hydrated, setHydrated] = React.useState(false);
+
+  // on user Login redirect to dashboard
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        logger(event, 'event');
+        logger(session, 'session');
+
+        if (event === 'SIGNED_IN') {
+          window.location.href = '/dashboard';
+        }
+      }
+    );
+
+    return () => {
+      authListener?.subscription.unsubscribe();
+    };
+  }, [supabase.auth]);
 
   React.useEffect(() => {
     setHydrated(true);
